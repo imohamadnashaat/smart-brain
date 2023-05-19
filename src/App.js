b77131253059012557142cebc952ae335a3b23d2
Mohamad Nashaat <imohamadnashaat@gmail.com>
2023-05-20 01:38:55 +0300

    Add rank & imageLinkForm components and install particles-bg package

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React from 'react';
+import ParticlesBg from 'particles-bg';
 
 import Navigation from './components/navigation/navigation.component';
 import Logo from './components/logo/logo.component';
+import Rank from './components/rank/rank.component';
+import ImageLinkForm from './components/image-link-form/image-link-form.component';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <ParticlesBg type="fountain" num={1} bg={true} />
       <Navigation />
       <Logo />
-      {/* 
+      <Rank />
       <ImageLinkForm />
+      {/* 
       <FaceRecognition />  
       */}
     </div>
