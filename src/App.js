3fec24b3dd7aec47bc41188041f25db36c57cc58
Mohamad Nashaat <imohamadnashaat@gmail.com>
2023-05-20 14:16:26 +0300

    Add onInputChange & onButtonSubmit event listener

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ParticlesBg from 'particles-bg';
 
 import Navigation from './components/navigation/navigation.component';
@@ -7,19 +7,39 @@ import Rank from './components/rank/rank.component';
 import ImageLinkForm from './components/image-link-form/image-link-form.component';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <ParticlesBg type="fountain" num={1} bg={true} />
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-      {/* 
-      <FaceRecognition />  
-      */}
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+    };
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  };
+
+  onButtonSubmit = () => {
+    console.log('Button clicked!');
+  };
+
+  render() {
+    return (
+      <div className="App">
+        <ParticlesBg type="fountain" num={1} bg={true} />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}
+        />
+        {/* 
+        <FaceRecognition />  
+        */}
+      </div>
+    );
+  }
 }
 
 export default App;
