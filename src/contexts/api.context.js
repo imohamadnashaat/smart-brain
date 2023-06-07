11cd17f3c99364078451fc46c6f575671058023a
Mohamad Nashaat <imohamadnashaat@gmail.com>
2023-06-07 18:41:27 +0300

    Update backend api url

--- src/contexts/api.context.js
+++ src/contexts/api.context.js
@@ -1,5 +1,3 @@
 import { createContext } from 'react';
 
-export const APIContext = createContext(
-  'https://smart-brain-api-yzoo.onrender.com/api'
-);
+export const APIContext = createContext('http://100.26.102.228:3000/api');
